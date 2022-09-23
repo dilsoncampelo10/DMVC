45afe3dd4f81b6a151c195d109dfbd8ee0f1d0f3
dilsoncampelo10 <dilsono316@gmail.com>
2022-09-23 16:59:19 -0300

    Criando classe de configurações

---
+++ core/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace core;
+
+session_start();
+
+class Config{
+
+const BASE = 'http://localhost:50/dmvc/public/';
+const DB_DRIVER = 'mysql';
+const DB_NAME = 'test';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+
+}
