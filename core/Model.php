c0210ea925d0c54d6f8153df236750fc9abe9a74
dilsoncampelo10 <dilsono316@gmail.com>
2022-09-23 18:46:34 -0300

    Criando model pai

---
+++ core/Model.php
@@ -0,0 +1,20 @@
+<?php
+namespace core;
+
+use core\Config;
+
+use PDO;
+use PDOException;
+
+class Model{
+    protected $con;
+    public function __construct()
+    {
+        try{
+            $this->con = new PDO(Config::DB_DRIVER.'dbname='.Config::DB_NAME.';'.Config::DB_HOST,Config::DB_USER,Config::DB_PASS); 
+        } catch(PDOException $e){
+            echo 'Problemas com conexão: '.$e->getMessage();
+        }
+         
+    }
+}
